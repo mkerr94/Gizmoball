881b1a6b30a5dd311040ca64da740a75db289e4b
Mark Kerr <mark.kerr.94@gmail.com>
2018-02-21 23:26:18 +0000

    template for model and gizmo interface

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,4 +1,16 @@
 package Model;
 
-public class Model {
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Model implements Observer {
+    private static final int L = 25;
+    private List<Gizmo> gizmos;
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 }
