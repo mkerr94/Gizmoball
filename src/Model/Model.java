55630a3c234516b1a2ad388c926837bec260b7cf
Mark Kerr <mark.kerr.94@gmail.com>
2018-02-21 23:33:20 +0000

    added physics library

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,5 +1,7 @@
 package Model;
 
+import Physics.LineSegment;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
@@ -8,6 +10,8 @@ import java.util.Observer;
 public class Model implements Observer {
     private static final int L = 25;
     private List<Gizmo> gizmos;
+    private List<LineSegment> lineSegments;
+
 
     @Override
     public void update(Observable o, Object arg) {
