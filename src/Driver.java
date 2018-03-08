cd7b6b940b6fba673dda45a7bda8bc877679c022
Mark Kerr <mark.kerr.94@gmail.com>
2018-03-08 14:59:07 +0000

    fixed file chooser bug

--- src/Driver.java
+++ src/Driver.java
@@ -8,8 +8,7 @@ public class Driver {
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             Model model = new Model();
-            LoadFile file = new LoadFile(model);
-            MainGUI view = new MainGUI(file, model);
+            MainGUI view = new MainGUI(model);
             view.setVisible(true);
         });
     }
