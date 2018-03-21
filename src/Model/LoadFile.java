bb25515b36b2381edc790e547e342d01bd41a728
fqb16193 <daniel.kashani.2016@uni.strath.ac.uk>
2018-03-21 00:52:08 +0000

    refactored

--- src/Model/LoadFile.java
+++ src/Model/LoadFile.java
@@ -19,7 +19,7 @@ public class LoadFile {
             e.printStackTrace();
         }
     }
-    void getLoadFile() throws FileNotFoundException {
+    private void getLoadFile() throws FileNotFoundException {
         int returnValue = fc.showOpenDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION){
             System.out.println("Success!");
@@ -38,8 +38,8 @@ public class LoadFile {
 
         int x = 0, y = 0, x1, y1, x2, y2, kNo;
         double d, d1, d2, d3;
-        String lineRead, gName, sConnect1, sConnect2, upDown;
-        IGizmo gConnect1, gConnect2;
+        String lineRead;
+        IGizmo gConnect1;
 
         try {
             FileReader fileReader = new FileReader(fc.getSelectedFile());
@@ -75,8 +75,6 @@ public class LoadFile {
                         model.addGizmo(new LeftFlipper(x, y));
                         break;
                     case "Absorber":
-                       //System.out.println("found");
-                        gName = gizmoGroup[1];
                         x1 = Integer.parseInt(gizmoGroup[2]);
                         y1 = Integer.parseInt(gizmoGroup[3]);
                         x2 = Integer.parseInt(gizmoGroup[4]);
@@ -84,7 +82,6 @@ public class LoadFile {
                         model.addGizmo(new Absorber(x1, y1, x2-x1, y2-y1));
                         break;
                     case "Ball":
-                        gName = gizmoGroup[1];
                         d = Double.parseDouble(gizmoGroup[2]);
                         d1 = Double.parseDouble(gizmoGroup[3]);
                         d2 = Double.parseDouble(gizmoGroup[4]);
@@ -100,8 +97,6 @@ public class LoadFile {
                         model.addBall(d, d1, d2, d3); // without * 30 it wont scale to L
                         break;
                     case "Rotate":
-                       // System.out.println("found Rotate");
-                        gName = gizmoGroup[1];
                         model.rotateGizmo(model.getGizmo(x, y));
                         break;
                         /*
@@ -111,22 +106,17 @@ public class LoadFile {
                     case "KeyConnect":
                         //System.out.println("found KeyConnect");
                         kNo = Integer.parseInt(gizmoGroup[2]);
-                        upDown = gizmoGroup[3];
                         gConnect1 = model.getGizmos().get(model.getGizmos().size()-1);;
                         //System.out.println("Should connect key " + kNo + " to gizmo " + gConnect1.toString());
                         model.addKeyConnection(kNo, gConnect1);
                         break;
                     case "Connect":
                         System.out.println("found Connect");
-                        sConnect1 = gizmoGroup[1];
-                        sConnect2 = gizmoGroup[2];
                         break;
 
                 }
                 lineRead = buffReader.readLine();
             }
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
         } catch (IOException e) {
             System.out.println(e);
         }
