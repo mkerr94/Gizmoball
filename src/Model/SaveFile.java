bcb56c71af4ea1d6c85dc7e62094b60a44a16693
fqb16193 <daniel.kashani.2016@uni.strath.ac.uk>
2018-03-21 00:35:04 +0000

    refactored

--- src/Model/SaveFile.java
+++ src/Model/SaveFile.java
@@ -11,19 +11,6 @@ import java.util.regex.Pattern;
 public class SaveFile {
 
     private Model model;
-    private int x;
-    private int y;
-    private int x2;
-    private int y2;
-    private int bIndex = 0;
-    private double bX;
-    private double bY;
-    private Vect velocity;
-    private String bV1 = "";
-    int i = 0;
-    private String gType;
-    private String gName;
-    private int rotate;
     private JFileChooser fc;
     private static final String FILE_PATH = System.getProperty("user.home") + "/Documents/load";
 
@@ -50,9 +37,6 @@ public class SaveFile {
         else if (returnValue == JFileChooser.CANCEL_OPTION){
             System.out.println("Cancelled, failed to pick an option");
         }
-        else if (JFileChooser.APPROVE_BUTTON_TEXT_CHANGED_PROPERTY == ""){
-            System.out.println("Failed to name your file");
-        }
     }
 
 
@@ -66,40 +50,41 @@ public class SaveFile {
         }
         assert fstream != null;
         BufferedWriter writer = new BufferedWriter(fstream);
+        String gType;
         for (IGizmo models : model.getGizmos()) {
-            x = models.getX1();
-            y = models.getY1();
+            int x = models.getX1();
+            int y = models.getY1();
             gType = models.getClass().getTypeName().substring(6);
-            gName = gType.substring(0, 1);
+            String gName = gType.substring(0, 1);
             System.out.println("Gizmo: " + gType + " X: " + x + " Y:" + y + " and the models is " + models);
             //writer.write(gType + " name " + x + " " + y);
             if(gType.equals("Absorber")){
                 Absorber newA = (Absorber) models;
-                x2 = newA.getX2();
-                y2 = newA.getY2();
+                int x2 = newA.getX2();
+                int y2 = newA.getY2();
                 writer.write(gType + " " + gName + " " + x + " " + y + " " + x2 + " " + y2);
                 writer.newLine();
                 continue;
             }
             if(gType.equals("Triangle") && models.getRotation() != 0){
-                writer.write(gType + " " + gName + x + + y + " " + x + " " + y);
+                writer.write(gType + " " + gName + x + +y + " " + x + " " + y);
                 writer.newLine();
                 if(models.getRotation() == 2) {
                     writer.write("Rotate " + gName + x + +y);
                     writer.newLine();
                 }
-                writer.write("Rotate " + gName + x + + y);
+                writer.write("Rotate " + gName + x + +y);
                 writer.newLine();
                 continue;
             }
             if (models instanceof RightFlipper){
                 System.out.println("hjghgfghjghfhgfjhfjhghgjhghgghhghghgghhghg");
-                x = x-1;
-                writer.write(gType + " " + gName + x + + y + " " + x + " " + y);
+                x = x -1;
+                writer.write(gType + " " + gName + x + +y + " " + x + " " + y);
                 writer.newLine();
             }
 
-            writer.write(gType + " " + gName + x + + y + " " + x + " " + y);
+            writer.write(gType + " " + gName + x + +y + " " + x + " " + y);
             writer.newLine();
             if(model.stringKeyConnection(models) != null){
                 System.out.println("keyConnection found " + (model.stringKeyConnection(models)));
@@ -109,9 +94,10 @@ public class SaveFile {
         }
 
         for (Ball models : model.getBalls()) {
-            bX = models.getExactX();
-            bY = models.getExactY();
-            velocity = models.getVelo();
+            double bX = models.getExactX();
+            double bY = models.getExactY();
+            String bV1 = "";
+            Vect velocity = models.getVelo();
             String sVelocity = velocity.toString();
             Pattern p = Pattern.compile("(\\d+(?:\\.\\d))");
             Matcher m = p.matcher(sVelocity);
@@ -122,11 +108,8 @@ public class SaveFile {
 
             }
             gType = models.getClass().getTypeName().substring(6);
-            writer.write(gType + " B" + bIndex + " " + bX + " " + bY + " " + bV1);
+            writer.write(gType + " B" + bX + bY  + " " + bX + " " + bY + " " + bV1);
             writer.newLine();
-            bV1 = "";
-            bIndex++;
-            System.out.println("Ball: " + gType + " X: " + x + " Y:" + y + " Velocity = " + bV1);
             model.printKeyConnections();
         }
         writer.close();
